8d1bcd1b36a59a6fcc16ec597bfde9da55673d94
derec <derec@DESKTOP-K2CTGVN>
2026-05-30 18:42:38 +0200

    Clase creada en rama 1

---
+++ Proyecto2/src/p2/ClaseRama1.java
@@ -0,0 +1,13 @@
+package p2;
+
+public class ClaseRama1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+        System.out.println("Hamburguesa");
+        System.out.println("Pollo");
+	}
+
+}
